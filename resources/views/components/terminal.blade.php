1dc943734bde421a1f89646aeabdcda0f3434f6e
sawirricardo <sawir.ricardo@gmail.com>
2023-06-21 11:01:41 +0700

    refactor code

--- resources/views/components/terminal.blade.php
+++ resources/views/components/terminal.blade.php
@@ -4,10 +4,26 @@
     line: '',
     commands: [],
     commandPointer: 0,
+    isSelectingCommand: false,
     baseDir: null,
     workingDirectory: @js($workingDirectory),
     get tildeWorkingDir() {
         return this.workingDirectory.replace(this.baseDir, '~')
+    },
+    setCurrentLine(line = '') {
+        this.line = line;
+        this.term.write(`\x1b[2K\r`)
+        this.term.write(`${tildeWorkingDir} => $ ${line}`)
+    },
+    handleBackspace() {
+        if (this.line) {
+            this.line = this.line.slice(0, this.line.length - 1)
+            this.term.write(`\b \b`);
+        }
+    },
+    replaceToCrlf(text) {
+        if (/\r\n/.test(text)) return text;
+        return text.replace(/\n/g, '\r\n');
     }
 }" x-init="baseDir = workingDirectory
 
@@ -15,64 +31,63 @@ term = new Terminal({
     cursorBlink: true,
 });
 term.open($el);
-term.write(`${tildeWorkingDir} => $ `)
+setCurrentLine();
 term.onKey(({ key, domEvent }) => {
     if (domEvent.keyCode === 38) { // ArrowUp
-        if (commandPointer > 0) {
-            commandPointer--;
-            line = commands[commandPointer];
-            term.write(`\x1b[2K\r`); // Clear current line
-            term.write(`${tildeWorkingDir} => $ ${line}`);
+        if (commands.length == 0) return;
+        if (isSelectingCommand) {
+            if (commandPointer < commands.length - 1) {
+                commandPointer++
+            }
         }
+        if (!isSelectingCommand) {
+            isSelectingCommand = true;
+        }
+        setCurrentLine(commands[commandPointer])
+        return;
     }
     if (domEvent.keyCode === 40) { // ArrowDown
-        if (commandPointer < commands.length - 1) {
-            commandPointer++;
-            line = commands[commandPointer];
-            term.write(`\x1b[2K\r`); // Clear current line
-            term.write(`${tildeWorkingDir} => $ ${line}`);
-        } else if (commandPointer === commands.length - 1) {
-            commandPointer++;
-            line = ''; // Empty line for new command
-            term.write(`\x1b[2K\r`); // Clear current line
-            term.write(`${tildeWorkingDir} => $ `);
+        if (isSelectingCommand && commandPointer == 0) {
+            isSelectingCommand = false;
+            setCurrentLine();
+        }
+        if (commandPointer > 0) {
+            commandPointer--;
+            setCurrentLine(commands[commandPointer]);
         }
         return;
     }
     if (domEvent.keyCode === 13) { // Enter
-        if (line) {
-            fetch(@js($url), {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-CSRF-TOKEN': document.head.querySelector(`meta[name='csrf-token']`).content,
-                    },
-                    body: JSON.stringify({
-                        command: line,
-                        working_directory: workingDirectory
-                    })
-                }).then((res) => res.json())
-                .then((data) => {
-                    term.writeln(``);
-                    if (data.output) {
-                        commands = [...commands, line];
-                        term.writeln(data.output);
-                    }
-                    workingDirectory = data.working_directory;
-                    term.write(`${tildeWorkingDir} => $ `);
-                }).catch((error) => {
-                    // Handle error here
-                    console.error(error);
-                });
-        }
-        line = ''
+        if (!line) return;
+        commands = [line, ...commands];
+        term.writeln(``);
+        fetch(@js($url), {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': document.head.querySelector(`meta[name='csrf-token']`).content,
+                },
+                body: JSON.stringify({
+                    command: line,
+                    working_directory: workingDirectory
+                })
+            }).then((res) => {
+                if (res.ok) return res.json()
+                return Promise.reject(res);
+            })
+            .then((data) => {
+                if (data.output) {
+                    term.writeln(replaceToCrlf(data.output));
+                }
+                workingDirectory = data.working_directory;
+                setCurrentLine();
+            }).catch((e) => {
+                console.error(e);
+            });
         return;
     }
     if (domEvent.keyCode === 8) { //BackSpace
-        if (line) {
-            line = line.slice(0, line.length - 1);
-            term.write(`\b \b`);
-        }
+        handleBackspace()
         return;
     }
     line += key;
@@ -90,7 +105,9 @@ term.attachCustomKeyEventHandler((e) => {
     }
     //  ctrl + v for paste
     if (e.ctrlKey && e.code === `KeyV` && e.type === `keydown`) {
-        navigator.clipboard.readText().then(text => term.write(text))
+        navigator.clipboard.readText().then(text => {
+            term.write(replaceToCrlf(line))
+        })
         return false;
     };
     return true;
